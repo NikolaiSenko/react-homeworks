ca37b8fea94758b0eec60b28dd76448494f8231b
Senko Nikolai <kolya.senko333@gmail.com>
2021-12-06 14:03:42 +0300

    fulfilled dropdown task

---
+++ src/components/Dropdown/index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { useState } from "react";
+const INGREDIENTS = ['пеперони','анчоусы','ананасы','сыр чеддр','шампиньоны']
+const Dropdown = () => {
+  const [prove, setProve] = useState(false)
+  const element = INGREDIENTS.map((ing) =>{
+    return(
+    <p>{ing}</p>
+    )
+  })
+  return (
+    <div>
+      <h3 onClick={()=>setProve(!prove)}>Ингридиенты</h3>
+      {prove && element}
+    </div>
+  )
+};
+
+export default Dropdown;
