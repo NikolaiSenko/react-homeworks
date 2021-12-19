f659aed228e09bd928f8187c6d41342109764e51
Senko Nikolai <kolya.senko333@gmail.com>
2021-12-19 20:14:47 +0300

    resolve conflicts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-<<<<<<< Updated upstream
 import TimePicker from './components/TimePicker'
 ReactDOM.render(
   <React.StrictMode>
     <TimePicker/>
-=======
-import App from './navigation'
-import Sauces from "./components/Sauces"
-ReactDOM.render(
-  <React.StrictMode>
-    <Sauces/>
->>>>>>> Stashed changes
   </React.StrictMode>,
   document.getElementById("root")
 );
