1ddc9b1c116f0bf9e94a28b3e8ed442b0f289677
Senko Nikolai <kolya.senko333@gmail.com>
2021-12-21 11:41:43 +0300

    little refactor

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -5,12 +5,10 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Posts from "../components/Posts";
-import Post from "../components/Post";
 import NavBar from "../components/NavBar";
 import Figure from "../components/Figure";
 
-export default function App() {
+export default function Navigator() {
   return (
     <Router>
       <div>
@@ -22,12 +20,6 @@ export default function App() {
           <Route path='/'>
             <NavBar/>
           </Route>
-        {/* <Route path="/posts/:postId">
-          <Post/>
-          </Route>
-          <Route path="/posts">
-          <Posts />
-          </Route> */}
         </Switch>
       </div>
     </Router>
