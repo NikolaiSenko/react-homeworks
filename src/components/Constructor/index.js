e8ac510dd03a7935a16538a415d3a4c2232d8b91
Senko Nikolai <kolya.senko333@gmail.com>
2021-12-27 13:15:37 +0300

    add checkbox logic

--- src/components/Constructor/index.js
+++ src/components/Constructor/index.js
@@ -1,18 +1,24 @@
 import "./styles.css";
 import { useState } from "react";
 const INGREDIENTS = [
-  "пеперони",
-  "анчоусы",
-  "ананасы",
-  "сыр чеддр",
-  "шампиньоны",
+  "Пеперони",
+  "Анчоусы",
+  "Ананасы",
+  "Сыр чеддр",
+  "Шампиньоны",
+  "Перчик халапеньо",
+  "Опята",
+  "Ветчина",
 ];
 const Constructor = () => {
-  const [prove, setProve] = useState(false);
-  const element = INGREDIENTS.map((ing) => {
+  const [prove, setProve] = useState(INGREDIENTS.map(() => false));
+  const onIngredient = (position) => {
+    setProve(prove.map((el, i) => (i === position ? !el : el)));
+  };
+  const element = INGREDIENTS.map((ing, index) => {
     return (
-      <div className= 'ingredient' key={ing} onClick={() => setProve(!prove)}>
-        <span>{prove ? '✔️' : '❌'}</span>
+      <div className="ingredient" key={ing} onClick={() => onIngredient(index)}>
+        <span>{prove[index] ? "✔️" : "❌"}</span>
         <p>{ing}</p>
       </div>
     );
